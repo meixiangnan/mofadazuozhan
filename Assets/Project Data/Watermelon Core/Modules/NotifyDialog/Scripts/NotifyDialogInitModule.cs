3f4696d4cb106e9b29a2818a857577e501820ce3
meixiangnan <1277674170@qq.com>
2026-06-28 08:27:14 +0800

    1

--- Assets/Project Data/Watermelon Core/Modules/NotifyDialog/Scripts/NotifyDialogInitModule.cs
+++ Assets/Project Data/Watermelon Core/Modules/NotifyDialog/Scripts/NotifyDialogInitModule.cs
@@ -9,10 +9,28 @@ namespace Watermelon
 
         public override void CreateComponent(Initialiser Initialiser)
         {
-            GameObject canvasGameObject = Instantiate(canvas, Initialiser.InitialiserGameObject.transform);
+            GameObject canvasGameObject = Instantiate(canvas);
+            canvasGameObject.transform.SetParent(null);
             canvasGameObject.transform.localScale = Vector3.one;
             canvasGameObject.transform.localPosition = Vector3.zero;
             canvasGameObject.transform.localRotation = Quaternion.identity;
+            DontDestroyOnLoad(canvasGameObject);
+
+            Canvas c = canvasGameObject.GetComponent<Canvas>();
+            if (c != null)
+            {
+                c.renderMode = RenderMode.ScreenSpaceOverlay;
+                c.sortingOrder = 998;
+            }
+
+            UnityEngine.UI.CanvasScaler scaler = canvasGameObject.GetComponent<UnityEngine.UI.CanvasScaler>();
+            if (scaler != null)
+            {
+                scaler.uiScaleMode = UnityEngine.UI.CanvasScaler.ScaleMode.ScaleWithScreenSize;
+                scaler.referenceResolution = new Vector2(1080, 1920);
+                scaler.matchWidthOrHeight = 1f;
+            }
+
             canvasGameObject.GetComponent<NotifyDialog>().Initialise();
         }
 
